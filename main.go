6ae840401a77f8573c0ce0396dae39f10be0951c
Reetik Rajan <44093575+r4reetik@users.noreply.github.com>
2022-12-21 15:11:27 +0530

    fix: race condition

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func bruteforce(process *uint, key string, keyLen int) {
+func bruteforce(process *int, key string, keyLen int, mu *sync.RWMutex) {
 	privateKey, err := crypto.GenerateKey()
 	if err != nil {
 		log.Fatal(err)
@@ -27,34 +28,44 @@ func bruteforce(process *uint, key string, keyLen int) {
 
 	address := string(bytes.ToLower([]byte(crypto.PubkeyToAddress(*publicKeyECDSA).Hex())))
 
+	mu.Lock()
 	*process++
+	mu.Unlock()
 
 	if address[2:keyLen] == key {
-		fmt.Println("Public Addr:", address)
 		fmt.Println("Private Key:", hexutil.Encode(privateKeyBytes))
+		fmt.Println("Public Addr:", address)
 		fmt.Println("------------------------------------------------------------------")
 	}
 }
 
 func main() {
-	var count uint = 0
-	var process uint = 0
-	var lag uint = 0
-	var key string = os.Args[1]
-	var keyLen int = len(key) + 2
-	do := true
-	for {
-		lag = count - process
-		fmt.Printf("Count: %v, Process: %v, Lag: %v\r", count, process, lag)
+	mu := &sync.RWMutex{}
 
-		if lag > 9999 {
+	count := 0
+	process := 0
+	lag := 0
+	
+	key := string(bytes.ToLower([]byte(os.Args[1])))
+	keyLen := len(key) + 2
+	
+	do := true
+	
+	fmt.Println("------------------------------------------------------------------")
+	for {
+		mu.RLock()
+		lag = count - process
+		fmt.Printf("Count: %v, Process: %v, Lag: %v    \r", count, process, lag)
+		mu.RUnlock()
+
+		if lag > 99999 {
 			do = false
 		} else if lag < 10 {
 			do = true
 		}
 
 		if do {
-			go bruteforce(&process, key, keyLen)
+			go bruteforce(&process, key, keyLen, mu)
 			count++
 		}
 	}
